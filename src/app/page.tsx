b6b4fd05eb8217f40d67d3858cf97383a869f824
SikandarTahir23 <sikandar.tahir04@gmail.com>
2024-10-19 01:24:32 -0700

    porto

---
+++ src/app/page.tsx
@@ -0,0 +1,51 @@
+"use client"
+import { Typewriter } from 'react-simple-typewriter';
+
+export default async function Home() {
+  return (
+    <>
+      <main>
+        <section className="home section" id="home">
+          <div className="container">
+            <div className="intro">
+              <img
+                src="/imgs/alsiam.png"
+                alt="Al Siam Profile"
+                className="shadow-dark"
+              />
+              <h1>Sikandar Tahir</h1>
+              <p>
+                <Typewriter
+                  words={['Full-stack Developer', 'UI/UX Designer', 'Chatbot Integrator']}
+                  loop={false}
+                  cursor
+                  cursorStyle="_"
+                  typeSpeed={70}
+                  deleteSpeed={50}
+                  delaySpeed={1000}
+                />
+              </p>
+              <div className="social-links">
+                <a href="https://twitter.com/" target="_blank">
+                  <i className="fa fa-twitter" />
+                </a>
+                <a href="https://facebook.com/" target="_blank">
+                  <i className="fa fa-facebook" />
+                </a>
+                <a href="https://github.com/" target="_blank">
+                  <i className="fa fa-github" />
+                </a>
+                <a href="https://instagram.com/" target="_blank">
+                  <i className="fa fa-instagram" />
+                </a>
+                <a href="https://linkedin.com/in/" target="_blank">
+                  <i className="fa fa-linkedin" />
+                </a>
+              </div>
+            </div>
+          </div>
+        </section>
+      </main>
+    </>
+  );
+}
